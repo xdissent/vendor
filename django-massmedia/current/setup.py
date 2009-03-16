5ddf3aebcaa8e3d303b2fab3479843b337c91a46
Greg Thornton <xdissent@gmail.com>
2009-03-16 22:33:57 +0000

    enabling setuptools and find_packages

--- django-massmedia/current/setup.py
+++ django-massmedia/current/setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name = "django-massmedia",
@@ -6,5 +6,5 @@ setup(
     url = 'http://opensource.washingtontimes.com/projects/django-massmedia/',
     author = 'Justin Quick',
     description = 'Allows for site staff can upload and edit the media files through the site, and the filesystem is maintained in the background.',
-    packages = ['massmedia']
+    packages = find_packages()
 )
