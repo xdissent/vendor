265fde5b041f574c88730d3f0901f2b92099a0c1
Greg Thornton <xdissent@gmail.com>
2009-06-20 12:57:37 +0000

    importing svn r88

---
+++ django-massmedia/svn-r88/massmedia/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from models import GrabVideo
+
+urlpatterns = patterns('',
+    (r'^type/(?P<type>[-\w]+)/$', 'massmedia.views.list_by_type'),
+    (r'^collection/(?P<id>\d+)/(?P<type>[-\w]+)/$', 'massmedia.views.list_by_collection_by_type'),
+    (r'^collection/(?P<id>\d+)/$', 'massmedia.views.list_by_collection'),
+    (r'^widget/(?P<id>\d+)/(?P<type>[-\w]+)/$','massmedia.views.widget'),
+    url(r'^grab/(?P<object_id>\d+)/$','django.views.generic.list_detail.object_detail',{'queryset':GrabVideo.objects.filter(public=True)}),
+    (r'^grab/$','massmedia.views.grab_categorized'),
+    ('','list'),
+)
